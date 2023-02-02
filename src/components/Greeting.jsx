e8e6e9826b93ffbe6e72a07aa4b48b945a1f4e4c
kibirige <kibirigejohn005@gmail.com>
2023-02-02 13:36:58 +0300

    create the greetings component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchGreeting } from '../redux/greeting'
+
+const Greeting = () => {
+    const {greeting, status} = useSelector(state => state.greeting)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(status === 'idle') {
+            dispatch(fetchGreeting())
+        }
+    }, [status])
+
+  return (
+    <div>
+        <h1>{greeting.message}</h1>
+    </div>
+  )
+}
+
+export default Greeting
